7b0a5bcd0bece602b0b503044997af14e262b298
Isaac Batista <isaac@ragnaroknyc.com>
2023-01-06 16:10:42 +0000

    Create dropdown component: logical and styling

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -1,5 +1,62 @@
-const Dropdown = () => {
-  return <>Dropdown</>;
+import React, { useState } from "react";
+import './style.scss'
+
+const Dropdown = (props) => {
+  const {
+    optionSelected,
+    options,
+    setOption
+  } = props;
+  const [ dropdownIsOpen, setDropdownIsOpen ] = useState(false);
+
+  const showOptions = () => {
+    if (dropdownIsOpen && options && options.length > 0) {
+      return options.map((option) => {
+        const { name, value } = option;
+        const handleOnClick = () => {
+          setOption(value);
+          setDropdownIsOpen(false);
+        };
+        const optionSelectedFound = findOptionSelected();
+        return (
+          <div className={`dropdown-option ${optionSelectedFound && optionSelectedFound.value === option.value ? 'selected' : ''}`} onClick={handleOnClick} key={value}>
+            <img alt={name} src={require(`../../assets/${value}.png`)} />
+            <span>{name}</span>
+          </div>
+        );
+      });
+    }
+  };
+
+  const showOption = () => {
+    const optionSelectedFound = findOptionSelected();
+    if (optionSelectedFound) {
+      return (
+        <>
+          <img
+            alt={optionSelectedFound.name}
+            src={require(`../../assets/${optionSelectedFound.value}.png`)}
+          />
+          <span>{optionSelectedFound.name}</span>
+        </>
+      );
+    }
+    return <span>Select</span>; 
+  };
+
+  const findOptionSelected = () => options.find((o) => o.value === optionSelected);
+
+  return (
+    <div className="dropdown">
+      <div
+        className="dropdown-label"
+        onClick={() => setDropdownIsOpen(!dropdownIsOpen)}
+      >
+        {showOption()}
+      </div>
+      <div className="dropdown-options">{showOptions()}</div>
+    </div>
+  );
 };
 
 export { Dropdown };
--- src/components/toolbar/index.js
+++ src/components/toolbar/index.js
@@ -1,8 +1,48 @@
-import { Button, Dropdown } from '../index';
+import React, { useState } from "react";
+import { Button, Dropdown } from "../index";
+import "./style.scss";
 
-import "./style.scss"
+const currencyFromOptions = [
+  {
+    name: "BTC - Bitcoin",
+    value: "btc",
+  },
+  {
+    name: "ETH - Ethereum",
+    value: "eth",
+  },
+  {
+    name: "XRP - Ripple",
+    value: "xrp",
+  },
+  {
+    name: "LTC - Litcoin",
+    value: "ltc",
+  },
+];
+
+const currencyToOptions = [
+  {
+    name: "EUR - Euro",
+    value: "eur",
+  },
+  {
+    name: "USD - American Dollar",
+    value: "usd",
+  },
+  {
+    name: "GBP - Pound sterling",
+    value: "gbp",
+  },
+  {
+    name: "CAD - Canadian Dollar",
+    value: "cad",
+  }
+];
 
 const Toolbar = () => {
+  const [ currencyFrom, setCurrencyFrom ] = useState("");
+  const [ currencyTo, setCurrencyTo ] = useState("");
   return (
     <div className="toolbar">
       <div className="toolbar-header">
@@ -11,7 +51,11 @@ const Toolbar = () => {
       <div className="toolbar-container">
         <div className="toolbar-container-row">
           <label className="toolbar-container-row-label">Currency from</label>
-          <Dropdown />
+          <Dropdown
+            setOption={setCurrencyFrom}
+            options={currencyFromOptions}
+            optionSelected={currencyFrom}
+          />
         </div>
         <div className="toolbar-container-row">
           <label className="toolbar-container-row-label">Amount</label>
@@ -19,18 +63,18 @@ const Toolbar = () => {
         </div>
         <div className="toolbar-container-row">
           <label className="toolbar-container-row-label">Currency to</label>
-          <Dropdown />
+          <Dropdown
+            setOption={setCurrencyTo}
+            options={currencyToOptions}
+            optionSelected={currencyTo}
+          />
         </div>
         <div className="toolbar-container-row">
           <label className="toolbar-container-row-label">Amount</label>
           <input />
         </div>
         <div className="toolbar-container-row">
-          <Button
-            backgroundColor="#49CD5E"
-            color="#FFFFFF"
-            text="Save"
-          />
+          <Button backgroundColor="#49CD5E" color="#FFFFFF" text="Save" />
         </div>
       </div>
     </div>
