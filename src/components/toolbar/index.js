6c8bf18d538737ffff4de50aa02668ec3d494735
Isaac Batista <isaac@ragnaroknyc.com>
2023-01-07 12:01:10 +0000

    Add currencies constants

--- src/components/toolbar/index.js
+++ src/components/toolbar/index.js
@@ -1,44 +1,7 @@
 import React, { useState } from "react";
 import { Button, Dropdown } from "../index";
+import { currencyFromOptions, currencyToOptions } from '../../services/constants';
 import "./style.scss";
-
-const currencyFromOptions = [
-  {
-    name: "BTC - Bitcoin",
-    value: "btc",
-  },
-  {
-    name: "ETH - Ethereum",
-    value: "eth",
-  },
-  {
-    name: "XRP - Ripple",
-    value: "xrp",
-  },
-  {
-    name: "LTC - Litcoin",
-    value: "ltc",
-  },
-];
-
-const currencyToOptions = [
-  {
-    name: "EUR - Euro",
-    value: "eur",
-  },
-  {
-    name: "USD - American Dollar",
-    value: "usd",
-  },
-  {
-    name: "GBP - Pound sterling",
-    value: "gbp",
-  },
-  {
-    name: "CAD - Canadian Dollar",
-    value: "cad",
-  }
-];
 
 const Toolbar = () => {
   const [ currencyFrom, setCurrencyFrom ] = useState(currencyFromOptions[0].value);
---
+++ src/services/constants/index.js
@@ -0,0 +1,39 @@
+const currencyFromOptions = [
+  {
+    name: "BTC - Bitcoin",
+    value: "btc",
+  },
+  {
+    name: "ETH - Ethereum",
+    value: "eth",
+  },
+  {
+    name: "XRP - Ripple",
+    value: "xrp",
+  },
+  {
+    name: "LTC - Litcoin",
+    value: "ltc",
+  },
+];
+
+const currencyToOptions = [
+  {
+    name: "EUR - Euro",
+    value: "eur",
+  },
+  {
+    name: "USD - American Dollar",
+    value: "usd",
+  },
+  {
+    name: "GBP - Pound sterling",
+    value: "gbp",
+  },
+  {
+    name: "CAD - Canadian Dollar",
+    value: "cad",
+  },
+];
+
+export { currencyFromOptions, currencyToOptions };
