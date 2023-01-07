e51372e812a48c123b77b8be294fee666c67d1f3
Isaac Batista <isaac@ragnaroknyc.com>
2023-01-07 11:37:43 +0000

    Click outside dropdown event

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -1,9 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './style.scss'
 
-const Dropdown = ({ optionSelected, options, setOption }) => {
-  const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+/**
+ * @description
+ * @param {*} ref 
+ * @param {*} dropdownIsOpen 
+ * @param {*} setDropdownIsOpen 
+ */
+function useOutsideAlerter(ref, dropdownIsOpen, setDropdownIsOpen) {
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        dropdownIsOpen &&
+        ref.current &&
+        !ref.current.contains(event.target)
+      ) {
+        setDropdownIsOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, dropdownIsOpen, setDropdownIsOpen]);
+}
 
+/**
+ * @description Dropdown component
+ * @param {string} optionSelected
+ * @param {Array} options
+ * @param {Function} setOption
+ * @returns JSX
+ */
+const Dropdown = ({ optionSelected, options, setOption }) => {
+  //
+  const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+  //
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef, dropdownIsOpen, setDropdownIsOpen);
+
+  /**
+   * @description
+   * @returns JSX
+   */
   const showOptions = () => {
     if (dropdownIsOpen && options && options.length > 0) {
       return options.map((option) => {
@@ -31,6 +70,10 @@ const Dropdown = ({ optionSelected, options, setOption }) => {
     }
   };
 
+  /**
+   * @description
+   * @returns JSX
+   */
   const showOption = () => {
     const optionSelectedFound = findOptionSelected();
     if (optionSelectedFound) {
@@ -47,11 +90,15 @@ const Dropdown = ({ optionSelected, options, setOption }) => {
     return <span>Select</span>;
   };
 
+  /**
+   * @description
+   * @returns Object
+   */
   const findOptionSelected = () =>
     options.find((o) => o.value === optionSelected);
 
   return (
-    <div className="dropdown">
+    <div className="dropdown" ref={wrapperRef}>
       <div
         className="dropdown-label"
         onClick={() => setDropdownIsOpen(!dropdownIsOpen)}
