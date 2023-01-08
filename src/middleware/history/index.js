435cdf4182f54d8c856b8c26c11959c5cd3bfdb7
Isaac Batista <isaac@ragnaroknyc.com>
2023-01-08 15:13:21 +0000

    Add post history action

--- src/middleware/history/index.js
+++ src/middleware/history/index.js
@@ -18,8 +18,6 @@ export function getHistory() {
 }
 
 export function postHistory(history) {
-  console.log(history);
-  /*
   axios
     .post(`${API_URL}/history`, history)
     .then((response) => {
@@ -28,5 +26,4 @@ export function postHistory(history) {
     .catch((error) => {
       console.log(error);
     });
-  */ 
 }
